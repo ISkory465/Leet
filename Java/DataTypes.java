0b36f7c02529e76426bcae935a0d5d2ee0d35036
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-06-18 17:15:01 +0200

    Datatypes used in Java

---
+++ Java/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        Examples();      
+    }
+
+    public static void Examples() {
+        // integer types
+        byte aSingleByte = 100; //-128 to 127
+        short aSmallNumber = 20000; // -32.768 to 32.767
+        int anInteger = 2147483647;
+        long aLargeNumber = 922337283685477587L;
+
+
+        // decimal types
+        double aDouble = 1.79769313;
+        float aFloat = 3.4028F;
+
+        // booleans
+        boolean isWeekend = true;
+        boolean isWorkday = false;
+
+        // characters
+        char copyrightSymbol = '\u00A9';
+
+        System.out.println("This is a copyright symbol: " + copyrightSymbol);
+    }
+}
