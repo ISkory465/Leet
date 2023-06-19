c591b6b06f5b8ff6427f35f4a151e43480789425
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-06-19 18:31:01 +0200

    Update: comparison/relational and logical operators are added

--- Java/Operators.java
+++ Java/Operators.java
@@ -1,7 +1,13 @@
 public class Operators {
     public static void main(String[] args) {
         System.out.println("Hello");
+        // BasicMath();
+        // RelationalOperator();
+        LogicOperator();
+    }
 
+    private static void BasicMath() {
+        
         int number1 = 12;
         int number2 = 5;
 
@@ -26,5 +32,30 @@ public class Operators {
 
         // remainder (modulo / modulus)
         System.out.println(number1 % number2); // 2
+    } 
+
+    private static void RelationalOperator() {
+        int a = 10;
+        int b = 5;
+        boolean isEqual = (a == b);      // Equality
+        boolean isNotEqual = (a != b);   // Not Equal
+        boolean isGreaterThan = (a > b); // Greater Than
+        boolean isLessThan = (a < b);    // Less Than
+        boolean isGreaterOrEqual = (a >= b);  // Greater Than or Equal To
+        boolean isLessOrEqual = (a <= b);     // Less Than or Equal To
+        
+        System.out.println("Check if two number are equal: " + isEqual);
+
+    }
+
+    private static void LogicOperator() {
+        boolean x = true;
+        boolean y = false;
+        boolean result1 = x && y;   // Logical AND
+        boolean result2 = x || y;   // Logical OR
+        boolean result3 = !x;       // Logical NOT
+
+
+        System.out.println("AND operator check: " + result1 + " | OR: " + result2 + " | NOT: " + result3);
     }
 }
