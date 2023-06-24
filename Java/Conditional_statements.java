ca835df705a3e3e53853ac0c02f71645bd918f06
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-06-24 20:57:59 +0200

    If-else cases completed

--- Java/Conditional_statements.java
+++ Java/Conditional_statements.java
@@ -32,6 +32,17 @@ public class Conditional_statements {
 
         if (operation.equals("sum")){
             System.out.printf("The sum of the numbers %.2f + %.2f = %.2f \n", number1, number2, number1+number2);    
+        } else if (operation.equals("sub")){
+            System.out.printf("The subtraction of the numbers %.2f - %.2f = %.2f \n", number1, number2, number1-number2);    
+        } else if (operation.equals("mult")){
+            System.out.printf("The multiplication of the numbers %.2f * %.2f = %.2f \n", number1, number2, number1*number2);    
+        } else if (operation.equals("div")){
+            if (number2 == 0){
+                System.out.printf("You can not divide by zero, you, stupid! \n");
+            } else {
+                System.out.printf("The division of the numbers %.2f / %.2f = %.2f \n", number1, number2, number1/number2);   
+            }
+            
         } else {
             System.out.printf("The peration %s is not supported by the current software version, kek. \n", operation);
         }
