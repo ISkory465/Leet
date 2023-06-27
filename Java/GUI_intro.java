b9c47355d01898f41f9180f8db9abbbb03f63ad3
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-06-27 19:33:17 +0200

    Basic GUI-pop windows with user input

--- Java/GUI_intro.java
+++ Java/GUI_intro.java
@@ -13,5 +13,12 @@ public class GUI_intro {
 
     private static void Gui_Example() {
         String name = JOptionPane.showInputDialog("What is your name? ");
+        JOptionPane.showMessageDialog(null, "Hello, " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("What is your age?"));
+        JOptionPane.showMessageDialog(null, "You are " + age + " years old.");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("What is your height?"));
+        JOptionPane.showMessageDialog(null, "You are " + height + " freedom units tall.");
     }
 }
