3bdd999e03c96587d4b605d9df96c5ddb9e6f9dd
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-07-08 17:52:40 +0200

    Wrapper class shown

---
+++ Java/wrapper_class.java
@@ -0,0 +1,18 @@
+public class wrapper_class {
+    public static void main(String[] args) {
+        System.out.println("Main executable.");
+
+        // autoboxing
+        Boolean objBool = true;
+        Character objChar = '@';
+        Integer objInt = 123;
+        Double objDbl = 3.14;
+        String name = "Iurii";
+
+        // unboxing
+        if (objChar == '@') {
+            System.out.println("This is true.");
+        }
+
+    }
+}
