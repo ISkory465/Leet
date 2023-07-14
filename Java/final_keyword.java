0b275ebe25e74056ba3de390aa14bc2fd3495248
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-07-14 17:11:27 +0200

    final parameter explained; makes the variable immutable

---
+++ Java/final_keyword.java
@@ -0,0 +1,11 @@
+public class final_keyword {
+    public static void main(String[] args) {
+        System.out.println("main");
+
+        final double PI = 3.1459;
+
+        // pi = 4;
+
+        System.out.println(PI);
+    }
+}
