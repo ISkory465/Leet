a3729aaee79dae70772ea65d10a823da036cd34d
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-07-16 18:45:31 +0200

    Refactor: toString call changed to implicit

--- Java/OOP/Test.java
+++ Java/OOP/Test.java
@@ -4,6 +4,7 @@ package OOP;
 public class Test {
     public static void main(String[] args) {
         Ferrari portofino = new Ferrari("Portofino M", "Silver Metalic", 2023);
-        System.out.println(portofino.toString());
+        System.out.println(portofino);
+        
     }
 }
