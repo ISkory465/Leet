abfd52d3f8143677c4378abedaff64ca7b261c06
Iurii Skorokhod <skorokhod.iurii@gmail.com>
2023-07-19 17:37:15 +0200

    feat: Example of method overriding

---
+++ Java/OOP/overriding/Animal.java
@@ -0,0 +1,8 @@
+package OOP.overriding;
+
+public class Animal {
+    
+    void speak() {
+        System.out.println("Sounds from the forest");
+    }
+}
---
+++ Java/OOP/overriding/main.java
@@ -0,0 +1,12 @@
+package OOP.overriding;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        Dog dog = new Dog();
+        Animal animal = new Animal();
+
+        dog.speak();
+    }
+}
